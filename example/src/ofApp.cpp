fc307e27043d082ef075952da4a128f2bbecb460
Peter Kolski <peter@bildpeter.de>
2017-05-17 18:57:03 +0200

    Better visual output

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -4,15 +4,13 @@
 void ofApp::setup(){
     auto portToReciever = 9999;
 
-    sender1.setup( portToReciever, "Peter1" );
-//    sender2.setup( portToReciever, "Peter2" );
+    sender1.setup( portToReciever, "Peter new" );
     reciever.setup( portToReciever );
 }
 
 
 void ofApp::update(){
     sender1.update();
-//    sender2.update();
     reciever.update();
 }
 
@@ -24,16 +22,14 @@ void ofApp::draw(){
     auto deviceIP=std::string();
     auto deviceAmountFound = reciever.getDeviceAmountFound();
 
-    if ( deviceAmountFound )
-    {
-        deviceName = reciever.getDeviceName( 0 );
-        deviceIP = reciever.getDeviceIP( 0 );
-    }
+
 
     ofDrawBitmapStringHighlight( "Amount: " + ofToString( deviceAmountFound ), 100, 100 );
 
     for ( int i = 0; i < deviceAmountFound; i++ )
     {
+        deviceName = reciever.getDeviceName( i );
+        deviceIP = reciever.getDeviceIP( i );
         ofDrawBitmapStringHighlight( "Name " + ofToString( i )+ ": " + ofToString( deviceName ), 100, 140 + i*20);
         ofDrawBitmapStringHighlight( "IP " + ofToString( i )+ ": " + ofToString( deviceIP ), 300, 140 + i*20);
     }
