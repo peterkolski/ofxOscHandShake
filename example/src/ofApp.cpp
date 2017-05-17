151b0a5e6294dd105d20035614b4bb645aca25c3
Peter Kolski <peter@bildpeter.de>
2017-05-17 17:25:52 +0200

    First internal communication works

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -2,8 +2,8 @@
 
 
 void ofApp::setup(){
-    sender.setup( 8888, 9999, "RPi 1" );
-    reciever.setup( 9999, 8888 );
+    sender.setup( 9999, "RPi 1" );
+    reciever.setup( 9999 );
 }
 
 
@@ -16,7 +16,17 @@ void ofApp::update(){
 void ofApp::draw(){
     ofBackground( 255 );
 
-    ofDrawBitmapStringHighlight( ofToString( "Amount: " + reciever.getDeviceAmountFound() ), 100, 100 );
-//    ofDrawBitmapStringHighlight( ofToString( "Name 1: " + reciever.getDeviceName( 0 ) ), 100, 140 );
-//    ofDrawBitmapStringHighlight( ofToString( "IP 1: " + reciever.getDeviceIP( 0 ) ), 100, 180 );
+    auto deviceName=std::string();
+    auto deviceIP=std::string();
+    auto deviceAmountFound = reciever.getDeviceAmountFound();
+
+    if ( deviceAmountFound )
+    {
+        deviceName = reciever.getDeviceName( 0 );
+        deviceIP = reciever.getDeviceIP( 0 );
+    }
+
+    ofDrawBitmapStringHighlight( "Amount: " + ofToString( deviceAmountFound ), 100, 100 );
+    ofDrawBitmapStringHighlight( "Name 0: " + ofToString( deviceName ), 100, 140 );
+    ofDrawBitmapStringHighlight( "IP 0: " + ofToString( deviceIP ), 100, 180 );
 }
