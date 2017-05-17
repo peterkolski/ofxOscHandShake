06a3bbb39b22dd56e2ea22b2220b88e02688d3ac
Peter Kolski <peter@bildpeter.de>
2017-05-17 20:01:42 +0200

    Cleanup

--- src/ofxOscHandShakeSender.cpp
+++ src/ofxOscHandShakeSender.cpp
@@ -22,15 +22,9 @@ void ofxOscHandShakeSender::setup( const string deviceName, const int portSendTo
 
 void ofxOscHandShakeSender::update()
 {
-//    if ( !isReceived_ )
-//    {
-        // TODO receive and check if message arrived + set bool true
-        // only stop messaging if both sides send true
-        
-        ofxOscMessage   messageInfo;
-        messageInfo.setAddress( addressHandShake_ );
-        messageInfo.addStringArg( deviceName_ );
-        messageInfo.addIntArg( isReceived_ );
-        sender_.sendMessage( messageInfo );
-//    }
+    ofxOscMessage   messageInfo;
+    messageInfo.setAddress( addressHandShake_ );
+    messageInfo.addStringArg( deviceName_ );
+    messageInfo.addIntArg( isReceived_ );
+    sender_.sendMessage( messageInfo );
 }
