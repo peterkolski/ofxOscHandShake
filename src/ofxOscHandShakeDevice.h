d4f346bc1c0e0f34a184346f5f1cbc4b7d2a7ffb
Peter Kolski <peter@bildpeter.de>
2017-05-19 14:49:51 +0200

    Ports in the devices

--- src/ofxOscHandShakeDevice.h
+++ src/ofxOscHandShakeDevice.h
@@ -16,19 +16,22 @@ using namespace std;
 
 struct ofxOscHandShakeDevice
 {
-    ofxOscHandShakeDevice( const string &nameDevice_, const string &ip_ )
-            : nameDevice_( nameDevice_ ), ip_( ip_ ) {}
+    ofxOscHandShakeDevice( const string &nameDevice_, const string &ip_ ) : nameDevice_( nameDevice_ ), ip_( ip_ ) {}
+
+    ofxOscHandShakeDevice( const string &nameDevice, const string &ip, int portGettingMessage, int portSendingTo )
+            : nameDevice_( nameDevice ), ip_( ip ), portGettingMessage( portGettingMessage ),
+              portSendingTo_( portSendingTo ) {}
 
     const string &getNameDevice() const { return nameDevice_; }
     const string &getIp() const { return ip_; }
-//    int getPortRecieveing() const { return portRecieveing_; }
-//    int getPortSendingFrom() const { return portSendingFrom_; }
+    int getPortGettingMessage() const { return portGettingMessage; }
+    int getPortSendingTo() const { return portSendingTo_; }
 
 private:
     string  nameDevice_;
     string  ip_;
-//    int     portRecieveing_;
-//    int     portSendingFrom_;
+    int     portGettingMessage  = 0;
+    int     portSendingTo_      = 0;
 };
 
 #endif /* defined(__Sound__ofxOscHandShakeDevice__) */
